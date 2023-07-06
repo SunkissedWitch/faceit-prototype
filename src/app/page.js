9478dd5e657a479ede7d018178dde1d1cad346ba
AnnaPravdiukova <annagekata82@gmail.com>
2023-07-06 20:07:21 +0300

    fixed spaces

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='flex flex-col gap-y-12 w-full'>
+    <div className='flex flex-col gap-y-20 w-full'>
       <HeroComposition />
     </div>
   )
--- src/components/HeroComposition/index.jsx
+++ src/components/HeroComposition/index.jsx
@@ -4,9 +4,9 @@ import Image from 'next/image'
 
 export const HeroComposition = () => {
   return (
-    <div className="hero min-h-[26.5rem] bg-contain bg-no-repeat" style={{backgroundImage: `url(${bg?.src})`}}>
+    <div className="hero mt-24 min-h-[26.5rem] bg-contain bg-no-repeat" style={{backgroundImage: `url(${bg?.src})`}}>
       {/* <div className="hero-overlay bg-opacity-60"></div> */}
-      <div className="hero-content text-center">
+      <div className="hero-content text-center py-6">
         <div className="max-w-3xl flex flex-col gap-y-5">
           <p className="mb-5 text-2xl">Planning. <span className='text-primary'>Programming.</span> Result.</p>
           <h1 className="mb-5 text-6xl capitalize font-semibold">End-to-end software <span className='bg-gradient-to-r from-primary to-[#038F00] bg-clip-text text-transparent'>development</span> company</h1>
