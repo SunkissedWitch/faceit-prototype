fa9b9b55a8f50104bb38fcf25ec4768b45309b6d
AnnaPravdiukova <annagekata82@gmail.com>
2023-07-11 20:23:44 +0300

    edited description width

--- src/components/OurTeam/index.jsx
+++ src/components/OurTeam/index.jsx
@@ -12,7 +12,7 @@ export const OurTeam = () => {
     <>
       <div className="flex flex-col gap-y-4">
         <HeaderText>{text.title}</HeaderText>
-        <DescriptionText>{text.desc}</DescriptionText>
+        <DescriptionText className='w-[80%]'>{text.desc}</DescriptionText>
         <ClientButton />
         <div className='border border-secondary border-opacity-30 h-80 p-24 -mx-28 text-secondary text-opacity-60 text-center'>
           team slider
