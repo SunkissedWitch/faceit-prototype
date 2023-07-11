850a1a19767f33abffc2e16f948ebe4f02714758
AnnaPravdiukova <annagekata82@gmail.com>
2023-07-11 15:32:56 +0300

    changed animated gradient target

--- src/components/HeroComposition/index.jsx
+++ src/components/HeroComposition/index.jsx
@@ -12,18 +12,20 @@ export const HeroComposition = () => {
       {/* <div className="hero-overlay bg-opacity-60"></div> */}
       <div className="hero-content z-auto text-center py-6">
         <div className="max-w-3xl flex flex-col gap-y-5">
-          <div className="mb-5 text-2xl text-primary-content antialiased flex flex-row gap-2 mx-auto">
+          <div className="mb-5 text-2xl text-primary-content antialiased">
+            Planning. <span className='text-primary'>Programming.</span> Result.
+          </div>
+          <div className="mb-5 text-6xl text-primary-content font-semibold antialiased flex flex-row flex-wrap mx-auto justify-center gap-x-4">
             <span className='heading-line heading-line-first'>
-              <span className='heading-line-gradient'>Planning.</span>
+              <span className='heading-line-gradient'>End-to-end Software</span>
             </span>
             <span className='heading-line heading-line-second'>
-              <span className='heading-line-gradient'>Programming.</span>
+              <span className='heading-line-gradient'>Development</span>
             </span>
             <span className='heading-line heading-line-third'>
-              <span className='heading-line-gradient'>Result.</span>
+              <span className='heading-line-gradient'>Company</span>
             </span>
           </div>
-          <h1 className="mb-5 text-6xl text-primary-content capitalize font-semibold">End-to-end software <span className='bg-gradient-to-r from-primary to-[#038F00] bg-clip-text text-transparent'>development</span> company</h1>
           <button className="btn btn-ghost text-base text-primary font-normal leading-5 gap-x-2 max-w-max mx-auto">
             Contact Us
             <Image src={arrow} alt='' width={28} height={28} placeholder='empty' />
