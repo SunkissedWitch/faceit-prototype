c1284fd464c17907a7895feb12d7c7fa6cc09d2c
AnnaPravdiukova <annagekata82@gmail.com>
2023-07-18 13:25:25 +0300

    slowed accordion animation, added transition on hover partner logo

--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -24,13 +24,13 @@ export const Accordion = () => {
         return (
           <div key={`${id}-${step.title}`} className="collapse join-item border-y border-base-300">
             <input type="checkbox" name="my-accordion-4" checked={isChecked} onChange={switchChecked} value={id} />
-            <div className="collapse-title flex flex-row items-center px-1 py-8">
-              <div className={`${isChecked ? 'text-secondary pl-3' : 'text-secondary-focus'} text-xl w-1/4 transition-all`}>{step.id < 10 && '0'}{id}</div>
+            <div className="collapse-title duration-300 flex flex-row items-center px-1 py-8">
+              <div className={`${isChecked ? 'text-secondary pl-3' : 'text-secondary-focus'} text-xl w-1/4 transition-all  duration-300`}>{step.id < 10 && '0'}{id}</div>
               <AccordionHeaderText active={isChecked}>
                 {step.title.toLowerCase()}
               </AccordionHeaderText>
             </div>
-            <div className="collapse-content">
+            <div className="collapse-content duration-300">
               <div className='w-3/4 ml-auto flex flex-col gap-y-7 px-6 pb-14'>
                 <div className='flex flex-col gap-y-2'>
                   {step.mainText?.map((row, index) => (
--- src/components/PartnerItem/index.jsx
+++ src/components/PartnerItem/index.jsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export const PartnerItem = ({ partnerLogo, title }) => {
   return (
     <div className='flex flex-initial px-4 cursor-pointer mx-11 bg-base-100'>
-      <Image className='h-[52px] w-full transition-colors grayscale hover:filter-none' src={partnerLogo} alt={title} />
+      <Image className='h-[52px] w-full transition-all duration-300 grayscale hover:filter-none' src={partnerLogo} alt={title} />
     </div>
   )
 }
