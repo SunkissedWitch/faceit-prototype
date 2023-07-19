da251ecb60fab52bd753447953fff5b05c741b87
AnnaPravdiukova <annagekata82@gmail.com>
2023-07-19 20:22:19 +0300

    added animation on scroll event

--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -1,30 +1,23 @@
 "use client"
 
 import { useState, useRef, useEffect } from "react"
+import { useInViewport } from 'react-in-viewport';
 import { steps } from "./content"
 import { AccordionItem } from "@/components/AccordionItem"
 
 export const Accordion = () => {
   const [checked, setCheked] = useState(0)
-  const firstHover = useRef(0)
-  const ref = useRef()
+  const myRef = useRef()
+  const {
+    inViewport,
+    enterCount,
+  } = useInViewport(myRef, { threshold: 0.5 })
 
   useEffect(() => {
-    const handleHover = async (event) => {
-      if (firstHover.current === 0) {
-        firstHover.current = 1
-        return setCheked(event.target.value)
-      }
-    };
-
-    const element = ref.current;
-
-    element.addEventListener('mouseenter', handleHover);
-
-    return () => {
-      element.removeEventListener('mouseenter', handleHover);
+    if (inViewport && enterCount === 1) {
+      return setCheked('1')
     }
-  }, []);
+  }, [inViewport, enterCount])
 
   const switchChecked = (e) => {
     const { value } = e?.target
@@ -35,13 +28,12 @@ export const Accordion = () => {
   }
 
   return (
-    <div className="join join-vertical w-full rounded-none">
+    <div className="join join-vertical w-full rounded-none" ref={myRef}>
       <AccordionItem
         step={steps[0]}
         id={steps[0].id}
         isChecked={steps[0].id.toString() === checked}
         switchChecked={switchChecked}
-        ref={ref}
       />
 
       {steps.slice(1).map((step) => {
--- src/components/AccordionItem/index.jsx
+++ src/components/AccordionItem/index.jsx
@@ -1,35 +1,34 @@
 "use client"
 
-import { forwardRef } from "react"
 import { AccordionHeaderText } from "@/components/AccordionHeaderText"
 import { AccordionDescriptionText } from "@/components/AccordionDescriptionText"
 
-export const AccordionItem = forwardRef(({ step, isChecked, switchChecked, id }, ref) => {
+export const AccordionItem = ({ step, isChecked, switchChecked, id }) => {
   return (
-    <div className="collapse join-item border-y border-base-300">
-      <input type="checkbox" name="my-accordion-4" checked={isChecked} onChange={switchChecked} value={id} ref={ref} />
-      <div className="collapse-title duration-300 flex flex-row items-center px-1 py-8">
-        <div className={`${isChecked ? 'text-secondary pl-3' : 'text-secondary-focus'} text-xl w-1/4 transition-all  duration-300`}>{step.id < 10 && '0'}{id}</div>
-        <AccordionHeaderText active={isChecked}>
-          {step.title.toLowerCase()}
-        </AccordionHeaderText>
+    <div className="collapse ease-in duration-[600ms] join-item border-y border-base-300">
+      <input type="checkbox" name="services-accordion" checked={isChecked} onChange={switchChecked} value={id} />
+      <div className="collapse-title">
+        <div className="flex flex-row items-center px-1">
+          <div className={`${isChecked ? 'text-secondary' : 'text-secondary-focus'} text-xl w-1/4 transition-colors duration-[600ms]`}>{step.id < 10 && '0'}{id}</div>
+          <AccordionHeaderText active={isChecked} className={'transition-colors duration-[600ms]'}>
+            {step.title.toLowerCase()}
+          </AccordionHeaderText>
+        </div>
       </div>
-      <div className="collapse-content duration-300">
-        <div className='w-3/4 ml-auto flex flex-col gap-y-7 px-6 pb-14'>
+      <div className="collapse-content w-3/4 ml-auto flex flex-col gap-y-7 px-6">
+        {/* <div className='w-3/4 ml-auto flex flex-col gap-y-7 px-6 pb-14'> */}
           <div className='flex flex-col gap-y-2'>
             {step.mainText?.map((row, index) => (
               <AccordionDescriptionText key={`service-main-text-${index}`}>{row}</AccordionDescriptionText>
             ))}
           </div>
-          <div className='flex flex-col gap-y-2'>
+          <div className='flex flex-col gap-y-2 pb-14'>
             {step.secondaryText?.map((row, index) => (
               <AccordionDescriptionText key={`service-secondary-text-${index}`} className='text-secondary'>{row}</AccordionDescriptionText>
             ))}
           </div>
-        </div>
+        {/* </div> */}
       </div>
     </div>
   )
-})
-
-AccordionItem.displayName = 'servicesAccordeon'
+}
