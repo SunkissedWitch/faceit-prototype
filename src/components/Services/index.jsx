90545e9f23e053a04d59a2a9704c8ab724a63949
AnnaPravdiukova <annagekata82@gmail.com>
2023-07-24 19:54:44 +0300

    added gap space to Services section

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -4,14 +4,14 @@ import { HeaderText } from "@/components/HeaderText"
 
 export const Services = () => {
   return (
-    <>
-      <div className='flex flex-row justify-between gap-5 pt-32' id="services">
+    <section id="services" className="flex flex-col w-full gap-y-28">
+      <div className='flex flex-row justify-between gap-5 pt-32'>
         <HeaderText>Services</HeaderText>
         <DescriptionText className='w-3/4 font-light'>Our specialists use not only hi-tech programming approaches, but also monitor the latest trends being up to date and implement the best solutions into our work.</DescriptionText>
       </div>
       <div>
         <Accordion />
       </div>
-    </>
+    </section>
   )
 }
