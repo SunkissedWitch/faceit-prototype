db0a5e88fd75a426c6d85d3a32b6c3f3b1295845
AnnaPravdiukova <annagekata82@gmail.com>
2023-07-31 19:45:40 +0300

    styled mobile and responsive stats

--- src/components/Stats/index.jsx
+++ src/components/Stats/index.jsx
@@ -1,3 +1,5 @@
+import './styles.css'
+
 const staticStat = [
   {
     value: '9+',
@@ -19,11 +21,11 @@ const staticStat = [
 
 export const Stats = () => {
   return (
-    <div className="stats w-full">
+    <div className="stats stats-vertical sm:grid-cols-2 lg:grid-cols-none lg:stats-horizontal sm:gap-y-8 w-full">
       {staticStat.map((stat, index) => (
-        <div key={`${stat.value}-${index}`} className="stat">
-          <div className="stat-value text-[4rem] leading-none text-primary font-semibold text-center mb-4">{stat.value}</div>
-          <div className="stat-desc text-[1.32rem] leading-none text-primary-content capitalize text-center">{stat.desc.toLowerCase()}</div>
+        <div key={`${stat.value}-${index}`} className="stat py-8 lg:py-4 custom-medium-stats first:before:hidden before:bg-neutral dark:before:bg-base-300 border-transparent lg:before:hidden lg:border-neutral dark:lg:border-[#252525] sm:before:hidden lg:after:hidden odd:after:hidden after:hidden sm:after:block">
+          <div className="stat-value text-[3.4375rem] lg:text-[3.25rem] xl:text-[4rem] leading-none text-primary font-semibold text-center mb-4">{stat.value}</div>
+          <div className="stat-desc text-xl lg:text-[1.125rem] xl:text-[1.32rem] leading-none text-primary-content capitalize text-center">{stat.desc.toLowerCase()}</div>
         </div>
       ))}
     </div>
