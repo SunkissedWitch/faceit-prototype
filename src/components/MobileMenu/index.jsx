45b5ca4d47d952ae73d1c1445747b2c9786bdbc4
AnnaPravdiukova <annagekata82@gmail.com>
2023-08-02 19:40:38 +0300

    mobile menu with drawer

--- src/components/MobileMenu/index.jsx
+++ src/components/MobileMenu/index.jsx
@@ -1,33 +1,36 @@
-// "use client"
+"use client"
 
-// import { useState } from 'react'
+import { useState } from 'react'
 import { Hamburger, Close } from '@/components/HamburgerIcons'
 
-// const MobileMenuBtn = (props) => {
-//   return (
-//     <label tabIndex={0} className="btn btn-ghost btn-circle swap swap-rotate">
-//       <input type="checkbox" {...props} />
-//       <Hamburger className="swap-off fill-current" />
-//       <Close className="swap-on fill-current" />
-//     </label>
-//   )
-// }
-
 export const MobileMenu = ({ children }) => {
-  // const [open, setOpen] = useState(false)
-  // const toggleOpen = () => setOpen(prevState => !prevState)
+  const [open, setOpen] = useState(false)
+  const toggleOpen = () => setOpen(prevState => !prevState)
 
   return (
-    <div className='dropdown dropdown-end'>
-      <label tabIndex={0} className="btn btn-ghost btn-circle swap swap-rotate">
-        <input type="checkbox" className='' />
-        <Hamburger className="swap-off fill-current" />
-        <Close className="swap-on fill-current" />
-      </label>
-      <div className='menu-dropdown'>
-        <ul tabIndex={0} className={`menu dropdown-content mt-3 z-[1] p-4 shadow bg-base-100 rounded-box w-screen h-screen`}>
-          {children}
-        </ul>
+    <div className="md:hidden">
+      <div className="drawer drawer-end">
+        <input id="mobile-navbar-menu" type="checkbox" className="drawer-toggle peer" checked={open} onChange={toggleOpen} />
+        <div className="drawer-content peer-checked-[:nth-of-type(1)_&]:swap-active">
+          <label htmlFor="mobile-navbar-menu" tabIndex={0} className="drawer-button btn btn-ghost btn-circle swap swap-rotate z-10">
+            <Hamburger className="swap-off fill-current" />
+            <Close className="swap-on fill-current" />
+          </label>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="mobile-navbar-menu" className="drawer-overlay mt-20"></label>
+          <div className='px-4 mt-20 shadow-lg w-screen sm:max-w-xs h-[calc(100%-5rem)] bg-base-100 text-base-content [&_ul>*]:rounded-none flex flex-col justify-between'>
+            <ul className="menu join join-vertical px-0 [&_li>*]:rounded-none [&_li>*]:join-item [&_li>*]:border-collapse [&_li>*]:border-b [&_li>*]:border-base-300 [&_li>*]:last:border-b-0 [&_li>a]:py-[1.56rem]">
+              {/* Sidebar content here */}
+              {children}
+            </ul>
+            <ul className='menu join join-vertical rounded-none [&_li>*]:rounded-none border-t pt-7 border-base-300 font-light'>
+              <li><a className='py-2.5 px-0' href="mailto:info@faceit.com.ua" target='_blank'>info@faceit.com.ua</a></li>
+              <li><a className='py-2.5 px-0' href="#" target='_blank'>Instagram</a></li>
+              <li><a className='py-2.5 px-0' href="#" target='_blank'>Linked In</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,44 +4,25 @@ import { MobileMenu } from '@/components/MobileMenu'
 
 export const Navbar = () => {
   return (
-    // <div className="navbar z-[8000] fixed top-0 px-2 sm:px-12 md:px-24 bg-base-100 bg-opacity-80">
-    //     <div className='flex flex-auto flex-shrink-0'>
-    //       <button className="btn btn-ghost px-2 sm:px-6 btn-lg">
-    //         <Logo />
-    //       </button>
-    //     </div>
-    //   <ul className='menu menu-horizontal px-2 sm:px-6 text-secondary'>
-    //     {
-    //       MAIN_LINKS.map((link, index) => {
-    //         const { title, href } = link
-    //         return (
-    //           <li key={`MAIN-LINKS-${href}-${index}`} ><a href={href}>{title}</a></li>
-    //         )
-    //       })
-    //     }
-    //   </ul>
-    // </div>
     <div className="navbar z-[8000] fixed top-0 px-5 sm:px-10 md:px-14 lg:px-[7.5rem] bg-base-100 bg-opacity-80">
       <div className='w-full h-full absolute top-0 right-0 backdrop-blur-sm -z-[1]' />
       <div className="navbar-start flex flex-initial w-fit">
-        <button className="btn btn-ghost px-2 sm:px-6 btn-lg -ml-2 sm:-ml-6">
+        <button className="btn btn-ghost px-2 sm:px-6 btn-lg -ml-2 sm:-ml-6 z-10">
           <Logo />
         </button>
       </div>
       <div className="navbar-end flex-grow">
-        <div className="sm:hidden dropdown dropdown-end">
-          <MobileMenu>
-            {
-              MAIN_LINKS.map((link, index) => {
-                const { title, href } = link
-                return (
-                  <li key={`mobile-links-${href}-${index}`} ><a href={href}>{title}</a></li>
-                )
-              })
-            }
-          </MobileMenu>
-        </div>
-        <ul className='hidden sm:flex menu menu-horizontal px-2 sm:px-6 text-secondary'>
+        <MobileMenu>
+          {
+            MAIN_LINKS.map((link, index) => {
+              const { title, href } = link
+              return (
+                <li key={`mobile-links-${href}-${index}`}><a href={href}>{title}</a></li>
+              )
+            })
+          }
+        </MobileMenu>
+        <ul className='hidden md:flex menu menu-horizontal px-2 md:px-6 text-secondary'>
           {
             MAIN_LINKS.map((link, index) => {
               const { title, href } = link
