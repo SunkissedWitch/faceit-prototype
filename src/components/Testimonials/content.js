b11aa3a07542be79e46d2fdf7524ffe6f75aa5da
AnnaPravdiukova <annagekata82@gmail.com>
2023-08-10 18:49:35 +0300

    added loading more testimonials

--- src/components/Testimonials/content.js
+++ src/components/Testimonials/content.js
@@ -1,3 +1,9 @@
+import barrington from 'public/testimonials/barrington.jpeg'
+import laurel from 'public/testimonials/laurel.jpeg'
+import lechler from 'public/testimonials/lechler.jpeg'
+import maor from 'public/testimonials/maor.jpeg'
+import mytidis from 'public/testimonials/mytidis.jpeg'
+
 export const REVIEWS = [
   {
     id: 1,
@@ -5,7 +11,7 @@ export const REVIEWS = [
     lastName: 'Barrington',
     role: 'Co-founder Billing Fast LTD',
     text: `Face IT team has developed an application for mobile, which helps to keep accounting records. We were satisfied by their work, programmers of the company offered us a couple of interesting ideas that have made our application faster operation.`,
-    avatar: false
+    avatar: barrington
   },
   {
     id: 2,
@@ -13,7 +19,7 @@ export const REVIEWS = [
     lastName: 'Laurel',
     role: 'Owner of the consulting firm Laurel & Partners',
     text: `We cooperate with the FacelT for more than 4 years. We are a consulting company and we have a large customer base. Face IT team developed a database system and has always supported it, adding more and more new features.`,
-    avatar: false
+    avatar: laurel
   },
   {
     id: 3,
@@ -21,7 +27,7 @@ export const REVIEWS = [
     lastName: 'Lechler',
     role: 'Automotive Group',
     text: `Face IT helped us in developing an Automotive Group site for in-company usage. The guys from this agency are highly skilled programmers who writes high quality code and simply pleasure to work with.`,
-    avatar: false
+    avatar: lechler
   },
   {
     id: 4,
@@ -29,6 +35,26 @@ export const REVIEWS = [
     lastName: 'Mutidis',
     role: 'Dating Social Network',
     text: `When I contacted Face IT I needed a team for my project - DriveTag (it's a platform parsing content from popular social networks and using tag search), thus I decided to go for several RoR devs.`,
-    avatar: false
+    avatar: mytidis
   },
+  {
+    id: 5,
+    firstName: 'Rea',
+    lastName: 'Maor',
+    role: 'Senior Technical PM at BR Marketing',
+    text: `I worked with Face IT professionals on several key projects varies from High-availability API, PhoneGap projects,
+    database architecture and many other interesting projects more than one year, all this period the team no less than Perfect.
+
+    I would definitely recommend Face IT. They were able to think solutions out of the box and come with outstanding ideas for complex issues.
+  `,
+    avatar: maor
+  },
+  // {
+  //   id: 6,
+  //   firstName: 'Lorem',
+  //   lastName: 'Ipsum',
+  //   role: 'fish',
+  //   text: ``,
+  //   avatar: false
+  // },
 ]
--- src/components/Testimonials/index.jsx
+++ src/components/Testimonials/index.jsx
@@ -1,21 +1,52 @@
+"use client"
+
 import { HeaderText } from "@/components/HeaderText"
 import { ReviewCard } from "@/components/ReviewCard"
 import { REVIEWS } from "./content"
-import { ClientButton } from "./ClientButton"
+import { useEffect, useState } from "react"
+import { useMediaQuery } from "react-responsive"
+import { PrimaryButton } from "../PrimaryButton"
 
 export const Testimonials = () => {
+  const isMobile = useMediaQuery({ maxWidth: '767px'})
+  const [amount, setAmount] = useState(2)
+  const [currentReviews, setCurrentReviews] = useState(REVIEWS.slice(0, 2))
+
+  useEffect(() => {
+    if (isMobile) {
+      setAmount(2)
+      setCurrentReviews(REVIEWS.slice(0, 2))
+      return
+    }
+    setAmount(4)
+    setCurrentReviews(REVIEWS.slice(0, 4))
+  }, [isMobile])
+
+  const sliceNext = (start) => {
+    const end = start + amount
+    return REVIEWS.slice(start, end)
+  }
+
+  const showMoreReviews = () => {
+    const nextReviews = sliceNext(currentReviews.length)
+    return setCurrentReviews((initState) => ([
+      ...initState,
+      ...nextReviews
+    ]))
+  }
+
   return (
     <>
       <div className='flex flex-col gap-y-8 pt-20 mt-4 sm:mt-0 sm:pt-32' id="testimonials">
         <HeaderText className='mb-5 sm:mb-9'>What our clients are saying</HeaderText>
         <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-12'>
           {
-            REVIEWS.map((review) => (
+            currentReviews.map((review) => (
               <ReviewCard key={`review-card-${review.id}`} review={review} />
             ))
           }
         </div>
-        <ClientButton />
+        <PrimaryButton title={'Learn more Testimonials'} onClick={showMoreReviews} className={'px-0'} />
       </div>
     </>
   )
