ac9792d53c3b5e586e840d820a1139560b83bda6
Anna Pravdiukova <89976002+SunkissedWitch@users.noreply.github.com>
2024-04-23 17:25:22 +0300

    CTA actions (#2)

--- src/components/AboutUs/ClientButton.jsx
+++ src/components/AboutUs/ClientButton.jsx
@@ -3,7 +3,12 @@
 import { PrimaryButton } from "@/components/PrimaryButton"
 
 export const ClientButton = () => {
-  const handleClick = (e) => console.log('Get started today', e.target)
+  const handleClick = () => {
+    const elem = document.getElementById('contacts');
+    elem.scrollIntoView({
+      behavior: 'smooth'
+    })
+  }
 
   return (
     <PrimaryButton className="mx-auto mt-8 sm:mt-2" title={'Get started today'} onClick={handleClick}/>
--- src/components/HeroComposition/ClientButton.jsx
+++ src/components/HeroComposition/ClientButton.jsx
@@ -3,7 +3,12 @@
 import { PrimaryButton } from "@/components/PrimaryButton"
 
 export const ClientButton = () => {
-  const handleClick = (e) => console.log(e.target)
+  const handleClick = () => {
+    const elem = document.getElementById('contacts');
+    elem.scrollIntoView({
+      behavior: 'smooth'
+    })
+  }
 
   return (
     <PrimaryButton className="mx-auto" title={'Contact Us'} onClick={handleClick}/>
